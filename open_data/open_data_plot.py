caf5a104cd187b0b1fa260989f97f1871e174473
Sebastian Grill <sebastian.grill@gmx.at>
2022-11-01 18:19:01 +0100

    migrate from bokeh to plotly

--- open_data/open_data_plot.py
+++ open_data/open_data_plot.py
@@ -1,19 +1,13 @@
 import io
 from datetime import datetime
 
-import bokeh
+import plotly.graph_objects as go
 import polars as pl
 import requests
-from bokeh import plotting
-from bokeh.io import curdoc
-from bokeh.models import BasicTickFormatter, LinearAxis, Range1d, Title
-from bokeh.themes import Theme
-from COVID_plots.themes.dark_minimal_adapted import json as jt
+from plotly.subplots import make_subplots
 from scipy.stats import gmean
 
 requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = "ALL:@SECLEVEL=1"
-
-curdoc().theme = Theme(json=jt)
 
 
 class OpenDataPlot:
@@ -189,383 +183,192 @@ class OpenDataPlot:
 
         plot_frame = plot_frame.with_column(
             pl.when(pl.col("test_pos_percentage").is_infinite())
-            .then(pl.lit(float("nan")))
+            .then(None)
             .otherwise(pl.col("test_pos_percentage"))
             .keep_name()
         )
 
-        plot_frame = plot_frame.with_columns(
-            [
-                pl.col("test_pos_percentage")
-                .rolling_mean(7, center=True)
-                .alias("test_pos_percentage_smoothed"),
-                pl.col("tests") / 10**6,
-                pl.col("doses_per_day") / 10**4,
-                pl.col("doses_administered_cumulative") / 10**6,
-                pl.col("doses_per_day") / 10**5,
-                pl.col("first_doses") / 10**6,
-                pl.col("second_doses") / 10**6,
-                pl.col("third_doses") / 10**6,
-                pl.col("fourth_doses") / 10**6,
-                pl.col("five+_doses") / 10**6,
-            ]
+        plot_frame = plot_frame.with_column(
+            pl.col("test_pos_percentage")
+            .rolling_mean(7, center=True, min_periods=5)
+            .alias("test_pos_percentage_smoothed")
         )
 
-        self.plot_frame = plot_frame.collect().to_pandas().set_index("idx")
-
-    def calc_axis_min_max(self, column_name):
-        edge_margin = 0.1
-
-        plot_frame = self.plot_frame
-        col_max = plot_frame[column_name].max()
-        col_min = plot_frame[column_name].min()
-        diff = col_max - col_min
-
-        ax_min = col_min - diff * edge_margin
-        ax_max = col_max + diff * edge_margin
-
-        return ax_min, ax_max
+        self.plot_frame = plot_frame.collect()
 
     def vac_vs_infection_plot(self):
-        source = bokeh.models.sources.ColumnDataSource(self.plot_frame)
+        low_opacity = 0.3
 
-        aspect = 10
-        low_alpha = 0.3
-
-        fig0 = bokeh.plotting.figure(
-            x_axis_type="datetime",
-            y_axis_label="Relative\nchange-%",
-            aspect_ratio=aspect,
+        fig = make_subplots(
+            rows=5,
+            cols=1,
+            shared_xaxes=True,
+            subplot_titles=[
+                "Infection numbers relative change in %",
+                "Daily view",
+                "Tests",
+                "Vaccinations",
+                "Hospitalizations",
+            ],
+            specs=[
+                [{"secondary_y": False}],
+                [{"secondary_y": True}],
+                [{"secondary_y": True}],
+                [{"secondary_y": True}],
+                [{"secondary_y": True}],
+            ],
         )
 
-        fig0.xaxis.visible = False
-        fig0.y_range = Range1d(-15, 20)
-
-        fig0.add_layout(Title(text="Relative growth"), "above")
-
-        date = self.plot_frame.index[-1].date().strftime("%d-%m-%Y")
-
-        fig0.add_layout(
-            Title(
-                text=(f"COVID-19 report Austria (last update {date})"),
-                text_font_size="16pt",
-            ),
-            "above",
+        fig.update_layout(
+            title_text="COVID-19 Austria open data plot", template="plotly_dark"
         )
 
-        glyph1 = fig0.line(
-            x="idx",
-            y="rel_change",
-            source=source,
-            color="white",
-            # legend_label='pos_cases',
-            # line_dash=[3, 6],
-            alpha=low_alpha,
-            # name=column
+        fig.add_traces(
+            [
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("rel_change").to_series(),
+                    opacity=low_opacity,
+                    line=dict(width=1, color="white"),
+                    name="Relative change",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("change_smoothed").to_series(),
+                    line=dict(width=1, color="white"),
+                    name="Relative change smoothed",
+                ),
+            ],
+            rows=1,
+            cols=1,
+        )
+        fig.add_hline(y=0, row=1, col=1, line=dict(width=1), opacity=0.7)
+        fig.update_layout(yaxis1=dict(range=[-22, 22]))
+
+        fig.add_traces(
+            [
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("pos_cases").to_series(),
+                    opacity=low_opacity,
+                    line=dict(width=1, color="#ff7f0e"),
+                    name="Positive cases",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("7d_mean").to_series(),
+                    line=dict(width=1, color="#ff7f0e"),
+                    name="Positive cases smoothed",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("deaths").to_series(),
+                    opacity=low_opacity,
+                    line=dict(width=1, color="#d62728"),
+                    name="Deaths",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("7d_mean_deaths").to_series(),
+                    line=dict(width=1, color="#d62728"),
+                    name="Deaths smoothed",
+                ),
+            ],
+            secondary_ys=[False, False, True, True],
+            rows=2,
+            cols=1,
         )
 
-        glyph1 = fig0.line(
-            x="idx",
-            y="change_smoothed",
-            source=source,
-            color="white",
-            # legend_label='pos_cases',
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
+        fig.add_traces(
+            [
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("tests").to_series(),
+                    line=dict(width=1, color="#1f77b4"),
+                    name="Tests",
+                    fill="tozeroy",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("test_pos_percentage").to_series(),
+                    opacity=low_opacity,
+                    line=dict(width=1, color="#e377c2"),
+                    name="Test positive percentage",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select(
+                        "test_pos_percentage_smoothed"
+                    ).to_series(),
+                    line=dict(width=1, color="#e377c2"),
+                    name="Test positive percentage smoothed",
+                ),
+            ],
+            secondary_ys=[False, True, True],
+            rows=3,
+            cols=1,
         )
 
-        glyph1 = fig0.line(
-            x="idx",
-            y="zero",
-            source=source,
-            color="white",
-            # legend_label='pos_cases',
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
+        fig.add_traces(
+            [
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("doses_per_day").to_series(),
+                    line=dict(width=1, color="#1f77b4"),
+                    name="Doses administered per day",
+                    fill="tozeroy",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("first_doses").to_series(),
+                    line=dict(width=1, color="#ff7f0e"),
+                    name="First doses administered",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("second_doses").to_series(),
+                    line=dict(width=1, color="#2ca02c"),
+                    name="Second doses administered",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("third_doses").to_series(),
+                    line=dict(width=1, color="#d62728"),
+                    name="Third doses administered",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("fourth_doses").to_series(),
+                    line=dict(width=1, color="#9467bd"),
+                    name="Fourth doses administered",
+                ),
+            ],
+            secondary_ys=[False, True, True, True, True, True],
+            rows=4,
+            cols=1,
         )
 
-        fig1 = bokeh.plotting.figure(
-            title="Daily view",
-            x_axis_type="datetime",
-            y_axis_label="Positive cases\nper day",
-            aspect_ratio=aspect,
-            x_range=fig0.x_range,
+        fig.add_traces(
+            [
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("hospitalizations").to_series(),
+                    line=dict(width=1, color="#8c564b"),
+                    name="Patients hospitalized",
+                ),
+                go.Scatter(
+                    x=self.plot_frame.select("idx").to_series(),
+                    y=self.plot_frame.select("ICU").to_series(),
+                    line=dict(width=1, color="#17becf"),
+                    name="Patients in intensive care",
+                ),
+            ],
+            secondary_ys=[False, True],
+            rows=5,
+            cols=1,
         )
 
-        fig1_min, fig1_max = self.calc_axis_min_max("7d_mean")
-        fig1.y_range = Range1d(fig1_min, fig1_max)
-
-        fig1.add_layout(
-            LinearAxis(y_range_name="y2", axis_label="Deaths per day"), "right"
-        )
-
-        fig1_sec_min, fig1_sec_max = self.calc_axis_min_max("7d_mean_deaths")
-        fig1.extra_y_ranges = {"y2": Range1d(fig1_sec_min, fig1_sec_max)}
-
-        fig1.xaxis.visible = False
-
-        glyph1 = fig1.line(
-            x="idx",
-            y="pos_cases",
-            source=source,
-            color="orange",
-            # legend_label='pos_cases',
-            # line_dash=[3, 6],
-            alpha=low_alpha,
-            # name=column
-        )
-
-        glyph2 = fig1.line(
-            x="idx",
-            y="7d_mean",
-            source=source,
-            color="orange",
-            legend_label="New cases smoothed",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-        )
-
-        glyph3 = fig1.line(
-            x="idx",
-            y="deaths",
-            source=source,
-            color="red",
-            # legend_label='deaths',
-            # line_dash=[3, 6],
-            alpha=low_alpha,
-            # name=column
-            y_range_name="y2",
-        )
-
-        glyph = fig1.line(
-            x="idx",
-            y="7d_mean_deaths",
-            source=source,
-            color="red",
-            legend_label="New deaths smoothed (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        fig2 = bokeh.plotting.figure(
-            title="Testing",
-            x_axis_type="datetime",
-            x_range=fig0.x_range,
-            aspect_ratio=aspect,
-            y_axis_label="Million tests per day",
-        )
-        fig2.yaxis.formatter = BasicTickFormatter(use_scientific=False)
-        fig2.xaxis.visible = False
-
-        fig2_min, fig2_max = self.calc_axis_min_max("tests")
-        fig2.y_range = Range1d(fig2_min, fig2_max)
-
-        fig2.add_layout(
-            LinearAxis(y_range_name="y2", axis_label="Positive tests %"), "right"
-        )
-
-        fig2_sec_min, fig2_sec_max = self.calc_axis_min_max(
-            "test_pos_percentage_smoothed"
-        )
-
-        fig2.extra_y_ranges = {"y2": Range1d(fig2_sec_min, fig2_sec_max)}
-
-        glyph = fig2.varea(
-            x="idx",
-            y1="zero",
-            y2="tests",
-            source=source,
-            # color='grey',
-            legend_label="Tests",
-            # line_dash=[3, 6],
-            alpha=low_alpha,
-            # name=column
-        )
-
-        glyph = fig2.line(
-            x="idx",
-            y="test_pos_percentage",
-            source=source,
-            color="violet",
-            # line_dash=[3, 6],
-            alpha=low_alpha,
-            # name=column,
-            y_range_name="y2",
-        )
-
-        glyph = fig2.line(
-            x="idx",
-            y="test_pos_percentage_smoothed",
-            source=source,
-            color="violet",
-            legend_label="Positive tests % smoothed (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column,
-            y_range_name="y2",
-        )
-
-        fig3 = bokeh.plotting.figure(
-            title="Vaccinations",
-            x_axis_type="datetime",
-            x_range=fig0.x_range,
-            aspect_ratio=aspect,
-            y_axis_label="Tenthousand doses\nper day",
-        )
-
-        fig3_min, fig3_max = self.calc_axis_min_max("doses_per_day")
-        fig3.y_range = Range1d(fig3_min, fig3_max)
-
-        fig3.add_layout(
-            LinearAxis(y_range_name="y2", axis_label="Million doses\nadministered"),
-            "right",
-        )
-
-        fig3_sec_min, fig3_sec_max = self.calc_axis_min_max(
-            "doses_administered_cumulative"
-        )
-
-        fig3.extra_y_ranges = {"y2": Range1d(fig3_sec_min, fig3_sec_max)}
-        fig3.yaxis.formatter = BasicTickFormatter(use_scientific=False)
-        fig3.xaxis.visible = False
-
-        glyph = fig3.varea(
-            x="idx",
-            y1="zero",
-            y2="doses_per_day",
-            source=source,
-            # color='gray',
-            legend_label="Doses per day",
-            # line_dash=[3, 6],
-            alpha=low_alpha,
-            # name=column
-        )
-
-        glyph = fig3.line(
-            x="idx",
-            y="doses_administered_cumulative",
-            source=source,
-            color="green",
-            legend_label="Accumulated doses (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        glyph = fig3.line(
-            x="idx",
-            y="first_doses",
-            source=source,
-            color="red",
-            legend_label="First doses (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        glyph = fig3.line(
-            x="idx",
-            y="second_doses",
-            source=source,
-            color="orange",
-            legend_label="Second doses (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        glyph = fig3.line(
-            x="idx",
-            y="third_doses",
-            source=source,
-            color="cyan",
-            legend_label="Third doses (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        glyph = fig3.line(
-            x="idx",
-            y="fourth_doses",
-            source=source,
-            color="white",
-            legend_label="Fourth doses (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        glyph = fig3.line(
-            x="idx",
-            y="five+_doses",
-            source=source,
-            color="orange",
-            legend_label="Fifth or higher doses (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        fig4 = plotting.figure(
-            title="Hospitalization",
-            x_axis_type="datetime",
-            x_range=fig0.x_range,
-            aspect_ratio=aspect,
-            y_axis_label="Patients hospitalized",
-        )
-
-        fig4_min, fig4_max = self.calc_axis_min_max("hospitalizations")
-        fig4.y_range = Range1d(fig4_min, fig4_max)
-
-        fig4.add_layout(
-            LinearAxis(y_range_name="y2", axis_label="Patients in ICU"), "right"
-        )
-
-        fig4_sec_min, fig4_sec_max = self.calc_axis_min_max("ICU")
-
-        fig4.extra_y_ranges = {"y2": Range1d(fig4_sec_min, fig4_sec_max)}
-        fig4.yaxis.formatter = BasicTickFormatter(use_scientific=False)
-
-        glyph = fig4.line(
-            x="idx",
-            y="hospitalizations",
-            source=source,
-            color="brown",
-            legend_label="Hospitalizations",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-        )
-
-        glyph = fig4.line(
-            x="idx",
-            y="ICU",
-            source=source,
-            color="aquamarine",
-            legend_label="ICU patients (right)",
-            # line_dash=[3, 6],
-            alpha=1,
-            # name=column
-            y_range_name="y2",
-        )
-
-        plot = bokeh.layouts.gridplot(
-            [[fig0], [fig1], [fig2], [fig3], [fig4]], sizing_mode="scale_width"
-        )
-
-        plotting.show(plot)
+        fig.show()
 
     def run(self):
         self.prepare_data()
